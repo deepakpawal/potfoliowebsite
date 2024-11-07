e26c5469b9a2ef067f218228daac3d794fa90fb3
deepakpawal <deepakpawal0@gmail.com>
2024-11-07 18:13:02 +0530

    Added images and links

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ Below are some of the apps I have created. Feel free to contact me!
 """
 st.write(content2)
 
-col3,col4 = st.columns(2)
+col3,empty_col,col4 = st.columns([1.5,0.5,1.5])
 
 df=pd.read_csv('data.csv',sep=';')
 
@@ -33,7 +33,13 @@ df=pd.read_csv('data.csv',sep=';')
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/"+ row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
