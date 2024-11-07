64bd347d58c74ee0fd76024c9aaa66646cb14cea
deepakpawal <deepakpawal0@gmail.com>
2024-11-07 18:04:09 +0530

    Render app titles into columns

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
@@ -8,8 +9,31 @@ with col1:
     st.image("images/photo.png")
 
 with col2:
-    st.title("Deepaks Blog")
+    st.title("Deepak Pawal")
     content="""
-    Hi Iam Deepak Pawal
+    Hi Deepak Pawal! You are a Python developer, actively involved 
+    in helping clients with various issues and continuously learning
+    about the functionalities of your company's product, Voyager.
+    Your work likely involves troubleshooting, problem-solving, and
+    possibly even contributing to improving the product,making you 
+    a key resource for both technical support and development.
     """
     st.info(content)
+
+content2="""
+Below are some of the apps I have created. Feel free to contact me!
+"""
+st.write(content2)
+
+col3,col4 = st.columns(2)
+
+df=pd.read_csv('data.csv',sep=';')
+
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
