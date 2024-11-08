78e2c94e776cefd1023030c8fe25b0c544c166d4
deepakpawal <deepakpawal0@gmail.com>
2024-11-08 21:16:01 +0530

    Added email function and linked it to form

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,3 +1,15 @@
-import streamlit as st
 
-st.header("Contact Us")
+import streamlit as st
+from send_email import send_email
+
+#st.set_page_config(layout="wide")
+st.header("Contact Me")
+
+with st.form(key='email_forms'):
+    user_email= st.text_input("Your email address")
+    message = st.text_area("Your message")
+    message ='Subject: Portfolio Website Contact Me Page'+'\n'+message+'\n'+'My email is : '+user_email
+    button = st.form_submit_button("Submit")
+    if button:
+        send_email(message)
+        st.info("Your email was sent successfully!")
---
+++ send_email.py
@@ -0,0 +1,17 @@
+import smtplib, ssl
+
+
+def send_email(message):
+    host = 'smtp.gmail.com'
+    port = 465
+
+    password='flmbqmxyzfnyyiof'
+    username='deepakpawal7798@gmail.com'
+
+    receiver = "deepakpawal7798@gmail.com"
+
+    context= ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host,port,context = context) as server:
+        server.login(username,password)
+        server.sendmail(username,receiver,message)
